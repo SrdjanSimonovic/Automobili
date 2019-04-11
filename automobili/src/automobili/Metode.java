cc4f8d50dabd8114235f5bef14073a3d1ae57c19
oik <oik@LAPTOP-BNEH5OLG>
2019-04-11 19:17:59 +0200

    Ovo je nas prvi commit

---
+++ automobili/src/automobili/Metode.java
@@ -0,0 +1,26 @@
+package automobili;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Metode {
+
+	private Connection konektujSe(String baza) throws SQLException {
+		
+		final String url = "jdbc:mysql://localhost:3306/"+baza+":useSSL=false";
+		final String username = "root";
+		final String password = "root";
+		
+		return DriverManager.getConnection(url, username, password);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
